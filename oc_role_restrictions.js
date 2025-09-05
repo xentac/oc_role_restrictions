c06fc955fba1d5e170ec791907f1223b209547cc
Jason Chu <jchu@xentac.net>
2025-09-06 09:16:35 +1200

    Embed waitForKeyElements.js for Torn PDA's benefit

--- oc_role_restrictions.js
+++ oc_role_restrictions.js
@@ -6,7 +6,6 @@
 // @author       underko[3362751], xentac[3354782]
 // @match        https://www.torn.com/factions.php*
 // @grant        GM_xmlhttpRequest
-// @require      https://update.greasyfork.org/scripts/502635/1422102/waitForKeyElements-CoeJoder-fork.js
 // @connect      raw.githubusercontent.com
 // @license      MIT
 // ==/UserScript==
@@ -297,5 +296,95 @@
       "[OCRoleRestrictions] Couldn't find faction id, going with defaults.",
     );
     cb();
+  }
+
+  // Inserting dependency because Torn PDA can't handle @require
+  // ==UserScript==
+  // @version 1.3.0
+  // @name waitForKeyElements.js (CoeJoder fork)
+  // @description A utility function for userscripts that detects and handles AJAXed content.
+  // @namespace https://github.com/CoeJoder/waitForKeyElements.js
+  // @author CoeJoder
+  // @homepage https://github.com/CoeJoder/waitForKeyElements.js
+  // @source https://raw.githubusercontent.com/CoeJoder/waitForKeyElements.js/master/waitForKeyElements.js
+  //
+  // ==/UserScript==
+
+  /**
+   * A utility function for userscripts that detects and handles AJAXed content.
+   *
+   * @example
+   * waitForKeyElements("div.comments", (element) => {
+   *   element.innerHTML = "This text inserted by waitForKeyElements().";
+   * });
+   *
+   * waitForKeyElements(() => {
+   *   const iframe = document.querySelector('iframe');
+   *   if (iframe) {
+   *     const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+   *     return iframeDoc.querySelectorAll("div.comments");
+   *   }
+   *   return null;
+   * }, callbackFunc);
+   *
+   * @param {(string|function)} selectorOrFunction - The selector string or function.
+   * @param {function}          callback           - The callback function; takes a single DOM element as parameter.
+   *                                                 If returns true, element will be processed again on subsequent iterations.
+   * @param {boolean}           [waitOnce=true]    - Whether to stop after the first elements are found.
+   * @param {number}            [interval=300]     - The time (ms) to wait between iterations.
+   * @param {number}            [maxIntervals=-1]  - The max number of intervals to run (negative number for unlimited).
+   */
+  function waitForKeyElements(
+    selectorOrFunction,
+    callback,
+    waitOnce,
+    interval,
+    maxIntervals,
+  ) {
+    if (typeof waitOnce === "undefined") {
+      waitOnce = true;
+    }
+    if (typeof interval === "undefined") {
+      interval = 300;
+    }
+    if (typeof maxIntervals === "undefined") {
+      maxIntervals = -1;
+    }
+    if (typeof waitForKeyElements.namespace === "undefined") {
+      waitForKeyElements.namespace = Date.now().toString();
+    }
+    var targetNodes =
+      typeof selectorOrFunction === "function"
+        ? selectorOrFunction()
+        : document.querySelectorAll(selectorOrFunction);
+
+    var targetsFound = targetNodes && targetNodes.length > 0;
+    if (targetsFound) {
+      targetNodes.forEach(function (targetNode) {
+        var attrAlreadyFound = `data-userscript-${waitForKeyElements.namespace}-alreadyFound`;
+        var alreadyFound = targetNode.getAttribute(attrAlreadyFound) || false;
+        if (!alreadyFound) {
+          var cancelFound = callback(targetNode);
+          if (cancelFound) {
+            targetsFound = false;
+          } else {
+            targetNode.setAttribute(attrAlreadyFound, true);
+          }
+        }
+      });
+    }
+
+    if (maxIntervals !== 0 && !(targetsFound && waitOnce)) {
+      maxIntervals -= 1;
+      setTimeout(function () {
+        waitForKeyElements(
+          selectorOrFunction,
+          callback,
+          waitOnce,
+          interval,
+          maxIntervals,
+        );
+      }, interval);
+    }
   }
 })();
